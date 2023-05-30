c6a79a6e5b8d731b84ab67b9d939a800d3a1452f
Waqas93 <waqasanjum93@gmail.com>
2023-05-30 11:04:46 +0500

    Footer Add

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import {  Link } from 'react-router-dom'
 
-export default function Footer() {
+export default function App() {
   return (
-    <div>
-      Footer
-    </div>
-  )
+
+
+      <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+        © 2023 Copyright: {""}
+            <Link to='/'>CHEF'S FOOD Good Food Good Life</Link>
+      </div>
+
+  );
 }
